a09bcfdbd947040d4705c0a29a011cb1c7602fe7
Thomas Schaller <torkleyy@gmail.com>
2018-12-30 13:44:24 +0100

    Add existing shred::Dispatcher

---
+++ src/builder.rs
@@ -0,0 +1,328 @@
+use std::fmt;
+
+use fxhash::FxHashMap;
+use shred::{RunNow, System};
+
+use crate::Dispatcher;
+use crate::dispatcher::{SystemId, ThreadLocal};
+use crate::stage::StagesBuilder;
+
+/// Builder for the [`Dispatcher`].
+///
+/// [`Dispatcher`]: struct.Dispatcher.html
+///
+/// ## Barriers
+///
+/// Barriers are a way of sequentializing parts of
+/// the system execution. See `add_barrier()`/`with_barrier()`.
+///
+/// ## Examples
+///
+/// This is how you create a dispatcher with
+/// a shared thread pool:
+///
+/// ```rust
+/// # #![allow(unused)]
+/// #
+/// # extern crate shred;
+/// # #[macro_use]
+/// # extern crate shred_derive;
+/// # use shred::{Dispatcher, DispatcherBuilder, Read, System};
+/// # #[derive(Debug, Default)] struct Res;
+/// # #[derive(SystemData)] #[allow(unused)] struct Data<'a> { a: Read<'a, Res> }
+/// # struct Dummy;
+/// # impl<'a> System<'a> for Dummy {
+/// #   type SystemData = Data<'a>;
+/// #
+/// #   fn run(&mut self, _: Data<'a>) {}
+/// # }
+/// #
+/// # fn main() {
+/// # let system_a = Dummy;
+/// # let system_b = Dummy;
+/// # let system_c = Dummy;
+/// # let system_d = Dummy;
+/// # let system_e = Dummy;
+/// let dispatcher: Dispatcher = DispatcherBuilder::new()
+///     .with(system_a, "a", &[])
+///     .with(system_b, "b", &["a"]) // b depends on a
+///     .with(system_c, "c", &["a"]) // c also depends on a
+///     .with(system_d, "d", &[])
+///     .with(system_e, "e", &["c", "d"]) // e executes after c and d are finished
+///     .build();
+/// # }
+/// ```
+///
+/// Systems can be conditionally added by using the `add_` functions:
+///
+/// ```rust
+/// # #![allow(unused)]
+/// #
+/// # extern crate system_graph;
+/// # #[macro_use]
+/// # extern crate shred_derive;
+/// # use shred::{Read, System};
+/// # use system_graph::{Dispatcher, DispatcherBuilder};
+/// # #[derive(Debug, Default)] struct Res;
+/// # #[derive(SystemData)] #[allow(unused)] struct Data<'a> { a: Read<'a, Res> }
+/// # struct Dummy;
+/// # impl<'a> System<'a> for Dummy {
+/// #   type SystemData = Data<'a>;
+/// #
+/// #   fn run(&mut self, _: Data<'a>) {}
+/// # }
+/// #
+/// # fn main() {
+/// # let b_enabled = true;
+/// # let system_a = Dummy;
+/// # let system_b = Dummy;
+/// let mut builder = DispatcherBuilder::new()
+///     .with(system_a, "a", &[]);
+///
+/// if b_enabled {
+///    builder.add(system_b, "b", &[]);
+/// }
+///
+/// let dispatcher = builder.build();
+/// # }
+/// ```
+///
+#[derive(Default)]
+pub struct DispatcherBuilder<'a, 'b> {
+    current_id: usize,
+    map: FxHashMap<String, SystemId>,
+    stages_builder: StagesBuilder<'a>,
+    thread_local: ThreadLocal<'b>,
+    #[cfg(feature = "parallel")]
+    thread_pool: Option<::std::sync::Arc<::rayon::ThreadPool>>,
+}
+
+impl<'a, 'b> DispatcherBuilder<'a, 'b> {
+    /// Creates a new `DispatcherBuilder` by using the `Default` implementation.
+    ///
+    /// The default behaviour is to create a thread pool on `finish`.
+    /// If you already have a rayon `ThreadPool`, it's highly recommended to configure
+    /// this builder to use it with `with_pool` instead.
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// Adds a new system with a given name and a list of dependencies.
+    /// Please note that the dependency should be added before
+    /// you add the depending system.
+    ///
+    /// If you want to register systems which can not be specified as
+    /// dependencies, you can use `""` as their name, which will not panic
+    /// (using another name twice will).
+    ///
+    /// Same as [`add()`](struct.DispatcherBuilder.html#method.add), but
+    /// returns `self` to enable method chaining.
+    ///
+    /// # Panics
+    ///
+    /// * if the specified dependency does not exist
+    /// * if a system with the same name was already registered.
+    pub fn with<T>(mut self, system: T, name: &str, dep: &[&str]) -> Self
+    where
+        T: for<'c> System<'c> + Send + 'a,
+    {
+        self.add(system, name, dep);
+
+        self
+    }
+
+    /// Adds a new system with a given name and a list of dependencies.
+    /// Please note that the dependency should be added before
+    /// you add the depending system.
+    ///
+    /// If you want to register systems which can not be specified as
+    /// dependencies, you can use `""` as their name, which will not panic
+    /// (using another name twice will).
+    ///
+    /// # Panics
+    ///
+    /// * if the specified dependency does not exist
+    /// * if a system with the same name was already registered.
+    pub fn add<T>(&mut self, system: T, name: &str, dep: &[&str])
+    where
+        T: for<'c> System<'c> + Send + 'a,
+    {
+        use std::collections::hash_map::Entry;
+
+        let id = self.next_id();
+
+        let dependencies = dep.iter()
+            .map(|x| {
+                *self.map
+                    .get(*x)
+                    .expect(&format!("No such system registered (\"{}\")", *x))
+            })
+            .collect();
+
+        if name != "" {
+            if let Entry::Vacant(e) = self.map.entry(name.to_owned()) {
+                e.insert(id);
+            } else {
+                panic!(
+                    "Cannot insert multiple systems with the same name (\"{}\")",
+                    name
+                );
+            }
+        }
+
+        self.stages_builder.insert(dependencies, id, system);
+    }
+
+    /// Adds a new thread local system.
+    ///
+    /// Please only use this if your struct is not `Send` and `Sync`.
+    ///
+    /// Thread-local systems are dispatched in-order.
+    ///
+    /// Same as
+    /// [`add_thread_local()`](struct.DispatcherBuilder.html#method.add_thread_local),
+    /// but returns `self` to enable method chaining.
+    pub fn with_thread_local<T>(mut self, system: T) -> Self
+    where
+        T: for<'c> RunNow<'c> + 'b,
+    {
+        self.add_thread_local(system);
+
+        self
+    }
+
+    /// Adds a new thread local system.
+    ///
+    /// Please only use this if your struct is not `Send` and `Sync`.
+    ///
+    /// Thread-local systems are dispatched in-order.
+    pub fn add_thread_local<T>(&mut self, system: T)
+    where
+        T: for<'c> RunNow<'c> + 'b,
+    {
+        self.thread_local.push(Box::new(system));
+    }
+
+    /// Inserts a barrier which assures that all systems
+    /// added before the barrier are executed before the ones
+    /// after this barrier.
+    ///
+    /// Does nothing if there were no systems added
+    /// since the last call to `add_barrier()`/`with_barrier()`.
+    ///
+    /// Thread-local systems are not affected by barriers;
+    /// they're always executed at the end.
+    ///
+    /// Same as
+    /// [`add_barrier()`](struct.DispatcherBuilder.html#method.add_barrier),
+    /// but returns `self` to enable method chaining.
+    pub fn with_barrier(mut self) -> Self {
+        self.add_barrier();
+
+        self
+    }
+
+    /// Inserts a barrier which assures that all systems
+    /// added before the barrier are executed before the ones
+    /// after this barrier.
+    ///
+    /// Does nothing if there were no systems added
+    /// since the last call to `add_barrier()`/`with_barrier()`.
+    ///
+    /// Thread-local systems are not affected by barriers;
+    /// they're always executed at the end.
+    pub fn add_barrier(&mut self) {
+        self.stages_builder.add_barrier();
+    }
+
+    /// Attach a rayon thread pool to the builder
+    /// and use that instead of creating one.
+    ///
+    /// Same as
+    /// [`add_pool()`](struct.DispatcherBuilder.html#method.add_pool),
+    /// but returns `self` to enable method chaining.
+    #[cfg(feature = "parallel")]
+    pub fn with_pool(mut self, pool: ::std::sync::Arc<::rayon::ThreadPool>) -> Self {
+        self.add_pool(pool);
+
+        self
+    }
+
+    /// Attach a rayon thread pool to the builder
+    /// and use that instead of creating one.
+    #[cfg(feature = "parallel")]
+    pub fn add_pool(&mut self, pool: ::std::sync::Arc<::rayon::ThreadPool>) {
+        self.thread_pool = Some(pool);
+    }
+
+    /// Prints the equivalent system graph
+    /// that can be easily used to get the graph using the `seq!` and `par!`
+    /// macros. This is only recommended for advanced users.
+    pub fn print_par_seq(&self) {
+        println!("{:#?}", self);
+    }
+
+    /// Builds the `Dispatcher`.
+    ///
+    /// In the future, this method will
+    /// precompute useful information in
+    /// order to speed up dispatching.
+    pub fn build(self) -> Dispatcher<'a, 'b> {
+        use crate::dispatcher::new_dispatcher;
+
+        #[cfg(feature = "parallel")]
+        let d = new_dispatcher(
+            self.stages_builder.build(),
+            self.thread_local,
+            self.thread_pool.unwrap_or_else(Self::create_thread_pool),
+        );
+
+        #[cfg(not(feature = "parallel"))]
+        let d = new_dispatcher(self.stages_builder.build(), self.thread_local);
+
+        d
+    }
+
+    fn next_id(&mut self) -> SystemId {
+        let id = self.current_id;
+        self.current_id += 1;
+
+        SystemId(id)
+    }
+
+    #[cfg(feature = "parallel")]
+    fn create_thread_pool() -> ::std::sync::Arc<::rayon::ThreadPool> {
+        use rayon::ThreadPoolBuilder;
+        use std::sync::Arc;
+
+        Arc::new(
+            ThreadPoolBuilder::new()
+                .build()
+                .expect("Invalid configuration"),
+        )
+    }
+}
+
+#[cfg(feature = "parallel")]
+impl<'b> DispatcherBuilder<'static, 'b> {
+    /// Builds an async dispatcher.
+    ///
+    /// It does not allow non-static types and accepts a `Resources` struct or a value that
+    /// can be borrowed as `Resources`.
+    pub fn build_async<R>(self, res: R) -> ::dispatch::async_dispatcher::AsyncDispatcher<'b, R> {
+        use crate::async_dispatcher::new_async;
+
+        new_async(
+            res,
+            self.stages_builder.build(),
+            self.thread_local,
+            self.thread_pool.unwrap_or_else(Self::create_thread_pool),
+        )
+    }
+}
+
+impl<'a, 'b> fmt::Debug for DispatcherBuilder<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.stages_builder.write_par_seq(f, &self.map)
+    }
+}
---
+++ src/dispatcher.rs
@@ -0,0 +1,232 @@
+use smallvec::SmallVec;
+
+use shred::RunNow;
+use shred::Resources;
+
+use crate::stage::Stage;
+
+/// The dispatcher struct, allowing
+/// systems to be executed in parallel.
+pub struct Dispatcher<'a, 'b> {
+    stages: Vec<Stage<'a>>,
+    thread_local: ThreadLocal<'b>,
+    #[cfg(feature = "parallel")]
+    thread_pool: ::std::sync::Arc<::rayon::ThreadPool>,
+}
+
+impl<'a, 'b> Dispatcher<'a, 'b> {
+    /// Sets up all the systems which means they are gonna add default values for the resources
+    /// they need.
+    pub fn setup(&mut self, res: &mut Resources) {
+        for stage in &mut self.stages {
+            stage.setup(res);
+        }
+
+        for sys in &mut self.thread_local {
+            sys.setup(res);
+        }
+    }
+
+    /// Dispatch all the systems with given resources and context
+    /// and then run thread local systems.
+    ///
+    /// This function automatically redirects to
+    ///
+    /// * [`dispatch_par`] in case it is supported
+    /// * [`dispatch_seq`] otherwise
+    ///
+    /// and runs `dispatch_thread_local` afterwards.
+    ///
+    /// Please note that this method assumes that no resource
+    /// is currently borrowed. If that's the case, it panics.
+    ///
+    /// [`dispatch_par`]: struct.Dispatcher.html#method.dispatch_par
+    /// [`dispatch_seq`]: struct.Dispatcher.html#method.dispatch_seq
+    pub fn dispatch(&mut self, res: &Resources) {
+        #[cfg(feature = "parallel")]
+        self.dispatch_par(res);
+
+        #[cfg(not(feature = "parallel"))]
+        self.dispatch_seq(res);
+
+        self.dispatch_thread_local(res);
+    }
+
+    /// Dispatches the systems (except thread local systems)
+    /// in parallel given the resources to operate on.
+    ///
+    /// This operation blocks the
+    /// executing thread.
+    ///
+    /// Only available with "parallel" feature enabled.
+    ///
+    /// Please note that this method assumes that no resource
+    /// is currently borrowed. If that's the case, it panics.
+    #[cfg(feature = "parallel")]
+    pub fn dispatch_par(&mut self, res: &Resources) {
+        let stages = &mut self.stages;
+
+        self.thread_pool.install(move || {
+            for stage in stages {
+                stage.execute(res);
+            }
+        });
+    }
+
+    /// Dispatches the systems (except thread local systems) sequentially.
+    ///
+    /// This is useful if parallel overhead is
+    /// too big or the platform does not support multithreading.
+    ///
+    /// Please note that this method assumes that no resource
+    /// is currently borrowed. If that's the case, it panics.
+    pub fn dispatch_seq(&mut self, res: &Resources) {
+        for stage in &mut self.stages {
+            stage.execute_seq(res);
+        }
+    }
+
+    /// Dispatch only thread local systems sequentially.
+    ///
+    /// Please note that this method assumes that no resource
+    /// is currently borrowed. If that's the case, it panics.
+    pub fn dispatch_thread_local(&mut self, res: &Resources) {
+        for sys in &mut self.thread_local {
+            sys.run_now(res);
+        }
+    }
+
+    /// This method returns the largest amount of threads this dispatcher
+    /// can make use of. This is mainly for debugging purposes so you can see
+    /// how well your systems can make use of multi-threading.
+    #[cfg(feature = "parallel")]
+    pub fn max_threads(&self) -> usize {
+        self.stages
+            .iter()
+            .map(|s| s.max_threads())
+            .fold(0, |highest, value| highest.max(value))
+    }
+}
+
+impl<'a, 'b, 'c> RunNow<'a> for Dispatcher<'b, 'c> {
+    fn run_now(&mut self, res: &Resources) {
+        self.dispatch(res);
+    }
+
+    fn setup(&mut self, res: &mut Resources) {
+        self.setup(res);
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct SystemId(pub usize);
+
+pub type SystemExecSend<'b> = Box<for<'a> RunNow<'a> + Send + 'b>;
+pub type ThreadLocal<'a> = SmallVec<[Box<for<'b> RunNow<'b> + 'a>; 4]>;
+
+#[cfg(feature = "parallel")]
+pub fn new_dispatcher<'a, 'b>(
+    stages: Vec<Stage<'a>>,
+    thread_local: ThreadLocal<'b>,
+    thread_pool: ::std::sync::Arc<::rayon::ThreadPool>,
+) -> Dispatcher<'a, 'b> {
+    Dispatcher {
+        stages,
+        thread_local,
+        thread_pool,
+    }
+}
+
+#[cfg(not(feature = "parallel"))]
+pub fn new_dispatcher<'a, 'b>(
+    stages: Vec<Stage<'a>>,
+    thread_local: ThreadLocal<'b>,
+) -> Dispatcher<'a, 'b> {
+    Dispatcher {
+        stages,
+        thread_local,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use shred::System;
+    use shred::Write;
+    use crate::DispatcherBuilder;
+
+    #[derive(Default)]
+    struct Res(i32);
+
+    struct Dummy(i32);
+
+    impl<'a> System<'a> for Dummy {
+        type SystemData = Write<'a, Res>;
+
+        fn run(&mut self, mut data: Self::SystemData) {
+            if self.0 == 4 {
+                // In second stage
+
+                assert_eq!(data.0, 6);
+            } else if self.0 == 5 {
+                // In second stage
+
+                assert_eq!(data.0, 10);
+            }
+
+            data.0 += self.0;
+        }
+    }
+
+    struct Panic;
+
+    impl<'a> System<'a> for Panic {
+        type SystemData = ();
+
+        fn run(&mut self, _: Self::SystemData) {
+            panic!("Propagated panic");
+        }
+    }
+
+    fn new_builder() -> DispatcherBuilder<'static, 'static> {
+        DispatcherBuilder::new()
+            .with(Dummy(0), "0", &[])
+            .with(Dummy(1), "1", &[])
+            .with(Dummy(2), "2", &[])
+            .with(Dummy(3), "3", &["1"])
+            .with_barrier()
+            .with(Dummy(4), "4", &[])
+            .with(Dummy(5), "5", &["4"])
+    }
+
+    fn new_resources() -> Resources {
+        let mut res = Resources::new();
+        res.insert(Res(0));
+
+        res
+    }
+
+    #[test]
+    #[should_panic(expected = "Propagated panic")]
+    fn dispatcher_panics() {
+        DispatcherBuilder::new()
+            .with(Panic, "p", &[])
+            .build()
+            .dispatch(&mut new_resources())
+    }
+
+    #[test]
+    fn stages() {
+        let mut d = new_builder().build();
+
+        d.dispatch(&mut new_resources());
+    }
+
+    #[test]
+    #[cfg(feature = "parallel")]
+    fn stages_async() {
+        let mut d = new_builder().build_async(new_resources());
+
+        d.dispatch();
+    }
+}
