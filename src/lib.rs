bf4472e601531b2cb04153f3f395e3d7354a7c1a
Thomas Schaller <torkleyy@gmail.com>
2019-04-05 12:15:04 +0200

    Remove async_dispatcher

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 
-#[cfg(feature = "parallel")]
-pub use self::async_dispatcher::AsyncDispatcher;
 pub use self::builder::DispatcherBuilder;
 pub use self::dispatcher::Dispatcher;
-#[cfg(feature = "parallel")]
-pub use self::par_seq::{Par, ParSeq, RunWithPool, Seq};
 
-#[cfg(feature = "parallel")]
-mod async_dispatcher;
 mod builder;
 mod dispatcher;
 mod stage;
